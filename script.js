d885102248020b122d4b623b53a207a4eb242d4e
dedev <denise.onea@gmail.com>
2019-05-10 16:24:05 +0200

    Four commit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// pour voir combien de personnes on cliqué sur le button CV pour le visualiser 
+// pour voir combien de personnes on cliqué sur le button CV pour le visualiser, le lier a google analytics 
 document.querySelector('button').addEventListener('click',(event) =>{
     console.log('CV Button was pressed');
 })
+// Alerts the tracking ID for the default tracker.
+ga(function(tracker) {
+    alert(tracker.get('trackingId'));
+});
